0992a2b4f127acf0f064d0f77d690bf92a4ef529
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-06-10 03:27:23 +0000

    set up express app with / and /new routes, access username in n req.body from the form

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.get("/", (req, res) => res.send(console.log("usernames will be logged here - wip")));
+app.get("/new", (req, res) => {
+    res.send(
+        '<form action="/new" method="POST">'+
+            '<label for="username">Username:</label>'+
+            '<input type="text" name="username" id="username">'+
+            '<button type="submit">Submit</button>'+
+        '</form>'
+    )
+});
+app.post("/new", (req, res) => res.send(console.log("username to be saved: ", req.body.username)))
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`My first Express app - listening on port ${PORT}!`);
+});
